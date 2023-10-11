1f96035ac62f114fa482ed74cfdf665f9e0315a1
Yingshi Liu <serendipper.liu@gmail.com>
2023-10-11 14:15:45 -0700

    render color picker

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
 // Write your Color component here
 
+import { useState } from 'react'
+
+
+const Color = ({color, setSelectedColor, curColor}) => {
+  const applyColor = curColor === color? color + " selected" : color;
+  return (
+    <div
+    className={applyColor}
+    onClick={() => setSelectedColor(color)} 
+    >
+    </div>
+  )
+}
 const App = () => {
+  const [selectedColor, setSelectedColor] = useState("")
   return (
     <div id="container">
       <div id="navbar">
         <div>Currently selected: </div>
-        <div className="red">red</div>
+        <div className={selectedColor}>{selectedColor}</div>
       </div>
-      <div id="colors-list">{/* colors go here */}</div>
+      <div id="colors-list">
+        <Color color="yellow" setSelectedColor={setSelectedColor} curColor={selectedColor}/>
+        <Color color="green" setSelectedColor={setSelectedColor} curColor={selectedColor}/>
+        <Color color="orange" setSelectedColor={setSelectedColor} curColor={selectedColor}/>
+      </div>
     </div>
   );
 };
-
 export default App;
